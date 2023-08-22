95abf66b0a161a1a14d11b0cc00c7c3864cc8616
hasa-a-a <hassan_ali_ahmad@outlook.com>
2023-08-22 19:53:23 +0300

    add new btn

--- Assets/JS/submit.js
+++ Assets/JS/submit.js
@@ -1,1 +1,8 @@
+let userEl = document.getElementById("User")
+let passEl = document.getElementById("Pass")
+let submitEl = document.getElementById("submit")
+let olduserEl = document.getElementById("olduser")
 document.title = "Register"
+olduserEl.addEventListener("click",function(){
+    window.location.replace("index.html")
+})
