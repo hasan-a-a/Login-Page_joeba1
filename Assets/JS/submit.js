b81a48528c290b4a366fa9c77be07eb9c6f9058e
hasa-a-a <hassan_ali_ahmad@outlook.com>
2023-08-23 20:45:21 +0300

    add confirm password

--- Assets/JS/submit.js
+++ Assets/JS/submit.js
@@ -1,5 +1,6 @@
-let userEl = document.getElementById("User")
-let passEl = document.getElementById("Pass")
+let registerUserEl = document.getElementById("registerUser")
+let registerPassEl = document.getElementById("registerPass")
+let registerconfEl = document.getElementById("registerconf")
 let submitEl = document.getElementById("submit")
 let olduserEl = document.getElementById("olduser")
 document.title = "Register"
